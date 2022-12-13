60b8dc40a4cacfe2d780a08fa3cf9360bc9ff1b8
Rasmus Pedersen <rasmuspeders1@gmail.com>
2022-12-13 21:40:36 +0100

    Solutions

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -6,8 +6,6 @@
 // The program should wait until all the spawned threads have finished and
 // should collect their return values into a vector.
 
-// I AM NOT DONE
-
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -15,16 +13,15 @@ fn main() {
     let mut handles = vec![];
     for i in 0..10 {
         handles.push(thread::spawn(move || {
-            let start = Instant::now();
             thread::sleep(Duration::from_millis(250));
             println!("thread {} is complete", i);
-            start.elapsed().as_millis()
         }));
     }
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+        handle.join();
+        completed_threads += 1;
     }
 
     if results.len() != 10 {
